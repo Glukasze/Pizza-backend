94eff53b727ff378fab14d4f31eb43b8ba5e6c7e
Glukasze <Mortal^66>
2021-05-14 19:18:44 +0200

    registration functionality -configured email sending with Mailtrap

--- src/main/java/com/pizza/service/EmailService.java
+++ src/main/java/com/pizza/service/EmailService.java
@@ -4,13 +4,15 @@ import com.pizza.model.email.EmailSender;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
 
 @Service
 @AllArgsConstructor
@@ -18,21 +20,50 @@ public class EmailService implements EmailSender {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
-    private final JavaMailSender mailSender;
-
     @Override
     @Async
     public void send(String to, String email) {
+
+        String from = "pizza@pizza.com";
+        String username = "260b80fd2ac9a1";
+        String password = "7a6c59b435e342";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.mailtrap.io");
+        prop.put("mail.smtp.port", "2525");
+        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
         try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
-            mimeMessageHelper.setText(email, true);
-            mimeMessageHelper.setTo(to);
-            mimeMessageHelper.setSubject("PIZZA: Confirm your email");
-            mimeMessageHelper.setFrom("gracjan.luk@gmail.com");
-            mailSender.send(mimeMessage);
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject("PIZZA: email confirmation");
+
+            String msg = email;
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+
         } catch (MessagingException e) {
-            LOGGER.error("failed to send mail", e);
+            LOGGER.error("failed to send email", e);
             throw new IllegalStateException("failed to send email");
         }
     }
